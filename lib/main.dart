d11042360387c647b2c838023436ddf08244d8e0
unknown <hakkisekerci@gmail.com>
2021-12-13 18:15:26 +0300

    add mp3

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,162 @@ void main() {
 }
 
 class MachineAudio extends StatelessWidget {
-  final cal = AudioCache();
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      body: SafeArea(
-        child: TextButton(
-            onPressed: () {
-              cal.play("4.mp3", volume: 5.0);
-            },
-            child: Container(
-              color: Colors.amber,
-            )),
+      home: Scaffold(
+        backgroundColor: Colors.black38,
+        body: PageVer(),
       ),
-    ));
+    );
+  }
+}
+
+class PageVer extends StatelessWidget {
+  final cal = AudioCache();
+  void muzikCal(String ses) {
+    cal.play("$ses.mp3");
+    Duration newDuration = Duration(milliseconds: 500);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: TextButton(
+                        onPressed: () {
+                          muzikCal("1");
+                        },
+                        child: Container(
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: TextButton(
+                        onPressed: () {
+                          muzikCal("2");
+                        },
+                        child: Container(
+                          color: Colors.orange,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: TextButton(
+                        onPressed: () {
+                          muzikCal("3");
+                        },
+                        child: Container(
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: TextButton(
+                        onPressed: () {
+                          muzikCal("4");
+                        },
+                        child: Container(
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: TextButton(
+                        onPressed: () {
+                          muzikCal("5");
+                        },
+                        child: Container(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: TextButton(
+                        onPressed: () {
+                          muzikCal("6");
+                        },
+                        child: Container(
+                          color: Colors.cyan,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: TextButton(
+                        onPressed: () {
+                          muzikCal("7");
+                        },
+                        child: Container(
+                          color: Colors.teal,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: TextButton(
+                        onPressed: () {
+                          muzikCal("8");
+                        },
+                        child: Container(
+                          color: Colors.indigo,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
